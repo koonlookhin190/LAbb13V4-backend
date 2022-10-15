4ed8a909b6d0ecc3c2b453ae45b97e462a4b9710
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-10-15 21:34:22 +0700

    Config CORS

--- src/main/java/se331/rest/LAbb13V4BackendApplication.java
+++ src/main/java/se331/rest/LAbb13V4BackendApplication.java
@@ -18,7 +18,7 @@ public class LAbb13V4BackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","http://localhost:8001")
                         .exposedHeaders("x-total-count");
 
             }
